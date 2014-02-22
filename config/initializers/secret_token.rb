c2c8bbf1d9fb78fa2edd9b9869857441cef65ce0
Jessica J. Kim <jessica@remind101.com>
2014-02-21 16:44:04 -0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Remind101::Application.config.secret_token = '2704fb7407fefaf8bfce5435cff9c1d5da1eb564ddfc22d29d54a9b8ff5b781894264835457e96e78916d3d0f451be54d2cd3a6b2c51572ac5a93a7ab839d7a9'
